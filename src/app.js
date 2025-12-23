9d7a56baeb50539bd861875017b0bbe7b49709d5
Mohamed Tamer <mohamed.tamer.mtn@gmail.com>
2025-12-23 02:44:34 +0200

    check now the updated ci

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ function greet(name) {
 
 module.exports = greet;
 
+// Run only if executed directly (not when required)
 if (require.main === module) {
   console.log(greet("Mohamed"));
 }
--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#src/test.sh
 EXPECTED="Hello, Test!"
 
 OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
@@ -9,6 +8,6 @@ if [ "$OUTPUT" == "$EXPECTED" ]; then
     echo "✅ Test passed!"
     exit 0
 else
-    echo "✕ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
+    echo "❌ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
     exit 1
 fi
